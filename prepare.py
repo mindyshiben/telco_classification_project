54354104616142e0e32a1b0f1f9d9e5832aba29c
Mindy Shiben <mindy.shiben@gmail.com>
2022-05-20 16:46:11 -0500

    project_preparation

--- prepare.py
+++ prepare.py
@@ -8,7 +8,7 @@ from sklearn.impute import SimpleImputer
 import acquire
 
 def prep_telco(df):
-    df = df.drop(['Unnamed: 0', 'customer_id'], axis=1)
+    df = df.drop(['Unnamed: 0',	'customer_id'], axis=1)
     df['total_charges'] = pd.to_numeric(df['total_charges'], errors='coerce')
     df.dropna(subset=['total_charges'], inplace=True)
     df['online_security'] = df['online_security'].replace({'No internet service':'No'})
@@ -18,19 +18,21 @@ def prep_telco(df):
     df['streaming_tv'] = df['streaming_tv'].replace({'No internet service':'No'})
     df['streaming_movies'] = df['streaming_movies'].replace({'No internet service':'No'})
     df['multiple_lines'] = df['multiple_lines'].replace({'No phone service':'No'})
-    dummy = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines',
-                  'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing',
-                  'churn', 'internet_service_type', 'payment_type', 'contract_type']], prefix= (['encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_']),
-              dummy_na=False, drop_first=[True, True, True, True, True, True, True, True, True, True, True, True, True])
-    dummy.rename(columns = {'gender_Male': 'e_gender_male', 'partner_Yes':'e_partner', 'dependents_Yes':'e_dependents',
-       'phone_service_Yes': 'e_phone_service', 'multiple_lines_Yes': 'e_multiple_lines',
-       'tech_support_Yes': 'e_tech_support', 'streaming_tv_Yes': 'e_tv_stream',
-       'streaming_movies_Yes': 'e_movies_stream',
-       'paperless_billing_Yes': 'e_paperless_bill', 'churn_Yes': 'e_churn',
-       'internet_service_type_Fiber optic' : 'e_fiber_optic', 'internet_service_type_None': 'e_no_internet',
-       'payment_type_Credit card (automatic)': 'e_cc_auto', 'payment_type_Electronic check': 'e_check_electric',
-       'payment_type_Mailed check': 'e_check_mail', 'contract_type_One year': 'e_oneyr',
-       'contract_type_Two year': 'e_twoyr'}, inplace = True)
+    dummy = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn', 'internet_service_type', 'payment_type', 'contract_type']],
+    dummy_na=False, drop_first=[True, True, True, True, True, True, True, True, True, True, True, True, True])
+    dummy.rename(columns = {'gender_Male': 'e_gender_male', 'partner_Yes':'e_partner',
+                                    'dependents_Yes':'e_dependents',
+            'phone_service_Yes': 'e_phone_service', 'multiple_lines_Yes': 'e_multiple_lines',
+            'tech_support_Yes': 'e_tech_support', 'streaming_tv_Yes': 'e_tv_stream',
+            'streaming_movies_Yes': 'e_movies_stream',
+            'paperless_billing_Yes': 'e_paperless_bill', 'churn_Yes': 'e_churn',
+            'internet_service_type_Fiber optic' : 'e_fiber_optic', 'internet_service_type_None': 'e_no_internet',
+            'payment_type_Credit card (automatic)': 'e_cc_auto', 'payment_type_Electronic check': 'e_check_electric',
+            'payment_type_Mailed check': 'e_check_mail', 'contract_type_One year': 'e_oneyr',
+            'contract_type_Two year': 'e_twoyr'}, inplace = True)
     df = pd.concat([dummy, df], axis=1)
     return df
+
     
+    
+
